d4246a7195aabbd151a220d02698bf82b728dcc7
Sergei Morozov <morozov@tut.by>
2017-03-26 17:38:44 -0700

    Trying to use Slim Framework. Twice more LOC in controller :-(

--- public/index.php
+++ public/index.php
@@ -2,34 +2,71 @@
 
 declare(strict_types=1);
 
-$autoload = __DIR__ . '/../vendor/autoload.php';
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+use \Slim\App;
 
-if (!file_exists($autoload)) {
-    echo 'You must set up the project dependencies, run the following commands:'
-        . PHP_EOL . 'curl -sS https://getcomposer.org/installer | php'
-        . PHP_EOL . 'php composer.phar install'
-        . PHP_EOL;
-    exit(1);
-}
-
-require $autoload;
-
-$controller = new \KiH\Controller(new \KiH\Client(
-    new \GuzzleHttp\Client(),
-    'https://onedrive.live.com/redir.aspx?cid=0b6c46ff0a72f8db&resid=B6C46FF0A72F8DB!196510&parId=B6C46FF0A72F8DB!116&authkey=!AB17lqCz5De3HEE'
-), new \KiH\Parser(), new \KiH\Generator(
-    'https://s11v.tk/kih'
-));
-
-try {
-    if ($_SERVER['REQUEST_URI'] === '/rss.xml') {
-        $controller->rss();
-    } elseif (preg_match('/\/media\/([^\/]+)\.mp3$/', $_SERVER['REQUEST_URI'], $matches)) {
-        $controller->download($matches[1]);
-    } else {
+if (php_sapi_name() == 'cli-server') {
+    $info = parse_url($_SERVER['REQUEST_URI']);
+    if (file_exists(__DIR__ . $info['path'])) {
         return false;
     }
-} catch (\KiH\Exception $e) {
-    fwrite(STDERR, $e->getMessage() . PHP_EOL);
-    return false;
+
+    // @link http://stackoverflow.com/questions/24336725/slim-framework-cannot-interpret-routes-with-dot
+    $_SERVER['SCRIPT_NAME'] = basename(__FILE__);
 }
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = new App([
+    'settings' => [
+        'displayErrorDetails' => true,
+    ],
+]);
+
+$container = $app->getContainer();
+$container['client'] = function () {
+    return new \KiH\Client(
+        new \GuzzleHttp\Client(),
+        'https://onedrive.live.com/redir.aspx?' . http_build_query([
+            'cid' => '0b6c46ff0a72f8db',
+            'resid' => 'B6C46FF0A72F8DB!196510',
+            'parId' => 'B6C46FF0A72F8DB!116',
+            'authkey' => '!AB17lqCz5De3HEE',
+        ])
+    );
+};
+
+$container['parser'] = function () {
+    return new \KiH\Parser();
+};
+
+$container['generator'] = function () {
+    return new \KiH\Generator('https://s11v.tk/kih');
+};
+
+$app->get('/rss.xml', function (Request $request, Response $response) {
+    $rss = $this->generator->generate(
+        $this->parser->parseFolder(
+            (string) $this->client->getFolder()
+        )
+    );
+
+    $response = $response
+        ->withHeader('Content-Type', 'text/xml; charset=UTF-8');
+
+    $response->getBody()->write($rss);
+
+    return $response;
+});
+$app->get('/media/{id}.mp3', function (Request $request, Response $response) {
+    $item = $this->parser->parseItem(
+        (string) $this->client->getItem(
+            $request->getAttribute('id')
+        )
+    );
+
+    return $response
+        ->withHeader('Location', $item['@content.downloadUrl']);
+});
+$app->run();
