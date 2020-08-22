55b2fa15ec934116162c499a6806b7cf656e6a90
Sergei Morozov <morozov@tut.by>
2020-08-22 12:02:34 -0700

    Restore the accidentally removed handling of static files

--- public/index.php
+++ public/index.php
@@ -5,6 +5,14 @@ declare(strict_types=1);
 use Psr\Container\ContainerInterface;
 use Slim\App;
 
+if (PHP_SAPI === 'cli-server') {
+    $info = parse_url($_SERVER['REQUEST_URI']);
+
+    if (file_exists(__DIR__ . $info['path'])) {
+        return false;
+    }
+}
+
 require __DIR__ . '/../vendor/autoload.php';
 
 (static function (): void {
